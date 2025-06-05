e96dbab75e5c99fd1d6f5706f1d4dca763ccd6fc
tsukasa <tsukasashinji@users.noreply.github.com>
2025-06-05 23:16:25 +0900

    修正

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 function greet() {
   console.log("Hello from the main branch!");
 }
+=======
+function greet() {
+    console.log("Hello from the local branch!");
+}
+>>>>>>> 00a38f4 (私達の修正)
